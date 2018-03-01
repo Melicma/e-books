e832478c19c0ef05ecce26f7cc489fff6e90189c
Martin Melichar <melicma1@fit.cvut.cz>
2018-03-01 15:40:04 +0100

    added translate dataTables

--- public/js/app.js
+++ public/js/app.js
@@ -3,5 +3,38 @@
  */
 
 $(document).ready( function () {
-    $('#tableWorks').DataTable();
+    $('#tableWorks').DataTable({
+        "columns": [
+            null,
+            null,
+            null,
+            null,
+            {"orderable": false},
+            {"orderable": false}
+        ],
+        "language": {
+            "decimal":        "",
+            "emptyTable":     "Žádné data k dispozici",
+            "info":           "Zobrazeno _START_ až _END_ z _TOTAL_ výsledků",
+            "infoEmpty":      "Zobrazeno 0 až 0 z 0 výsledků",
+            "infoFiltered":   "(filtrováno z _MAX_ celkových výsledků)",
+            "infoPostFix":    "",
+            "thousands":      ",",
+            "lengthMenu":     "Zobrazit _MENU_ záznamů",
+            "loadingRecords": "Načítání...",
+            "processing":     "Processing...",
+            "search":         "Vyhledat:",
+            "zeroRecords":    "Výsledek nenalezen",
+            "paginate": {
+                "first":      "První",
+                    "last":       "Poslední",
+                    "next":       "Další",
+                    "previous":   "Předchozí"
+            },
+            "aria": {
+                "sortAscending":  ": aktivovat pro třídění sloupců vzestupně",
+                    "sortDescending": ": aktivovat pro třídění sloupců sestupně"
+            }
+        }
+    });
 } );
