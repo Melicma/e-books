9407c689b1178f13299ec12550249ac335e15b70
Martin Melichar <melicma1@fit.cvut.cz>
2018-04-23 23:14:07 +0200

    added resizer BUT not tested
    i can't install GD library to my awesome notebook
    marked text in /text can be bolded

--- public/js/app.js
+++ public/js/app.js
@@ -107,9 +107,44 @@ $(document).ready( function () {
 
         $("label[for='" + this.id + "']").text(fileName);
     });
+
+    $.each($('textarea[data-autoresize]'), function() {
+        var offset = this.offsetHeight - this.clientHeight;
+
+        var resizeTextarea = function(el) {
+            var tmp = $(window).scrollTop();
+
+            $(el).css('height', 'auto').css('height', el.scrollHeight + offset);
+            $(window).scrollTop(tmp);
+        };
+        $(this).on('keyup input', function() { resizeTextarea(this); }).removeAttr('data-autoresize');
+    });
+
+    $("textarea").each(function () {
+        this.style.height = (this.scrollHeight+10)+'px';
+    });
+
+
+
 } );
 
 function doImgModal(id) {
     $('.imagepreview').attr('src', $('#myImg'+id).attr('src').replace('_small', ''));
     $('#imagemodal').modal('show');
+}
+
+function doBold() {
+    var editor = document.getElementById("texArea01");
+    console.log(editor.value);
+    var editorHTML = editor.value;
+    var selectionStart = 0, selectionEnd = 0;
+    if (editor.selectionStart) selectionStart = editor.selectionStart;
+    if (editor.selectionEnd) selectionEnd = editor.selectionEnd;
+    if (selectionStart != selectionEnd) {
+        var editorCharArray = editorHTML.split("");
+        editorCharArray.splice(selectionEnd, 0, "\</b>");
+        editorCharArray.splice(selectionStart, 0, "\<b>"); //must do End first
+        editorHTML = editorCharArray.join("");
+        editor.value = editorHTML;
+    }
 }
